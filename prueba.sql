ff6af5c5dbb003a5401cc9a62e75c34a39326763
Andrik0711 <mord.71gamer@gmail.com>
2024-07-03 21:38:09 -0600

    First Commit

---
+++ prueba.sql
@@ -0,0 +1,4 @@
+-- Parte 1 --
+
+-- 1. Escribe una consulta SQL para obtener los nombres y salarios de los desarrolladores que tienen un salario superior a 50000.
+SELECT nombre, salaria from empleados WHERE salario > 50000;
